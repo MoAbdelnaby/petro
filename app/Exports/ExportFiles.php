0c66b2b5e459ac1355545e2acbe1a53601891ce3
Hassan Elhawary <hm5342590@gmail.com>
2022-01-25 14:56:55 +0200

    handle test node

---
+++ app/Exports/ExportFiles.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Exports;
+
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+
+class ExportFiles implements FromCollection, WithHeadingRow, WithHeadings
+{
+    protected $result, $columns;
+
+    public function __construct($result)
+    {
+        $this->result = collect($result);
+    }
+
+    /**
+     * @return Collection
+     */
+    public function collection(): Collection
+    {
+        return ($this->result);
+    }
+
+    public function headings(): array
+    {
+        return [array_keys(collect($this->result)[0])];
+    }
+}
