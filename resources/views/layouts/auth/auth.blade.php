7943f0a999de82c12ea331088184d9b900d506a2
Hassan Elhawary <hm5342590@gmail.com>
2022-05-08 16:26:42 +0200

    add meta csrf in login

--- resources/views/layouts/auth/auth.blade.php
+++ resources/views/layouts/auth/auth.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}"/>
 
     @yield('meta')
     <title>{{__('app.website_name')}} | @yield('page_title',__('app.dashboard'))</title>
